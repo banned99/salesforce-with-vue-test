b96b432d87d8581fe429ef98aa559045a3ba95fe
Supakrit Poolperm <ken@cloudindustry.com>
2018-09-05 10:43:21 +0700

    edit .gitignore

---
+++ dist/precache-manifest.22b385a7fc4f13b7452fe9c9ce10c544.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0eaa66aec9c70667468e",
+    "url": "/js/chunk-vendors.28280c1e.js"
+  },
+  {
+    "revision": "817153ec89f160efb372",
+    "url": "/js/app.4aaaebc7.js"
+  },
+  {
+    "revision": "0cf5b2f1aff9fbfe02b2",
+    "url": "/js/about.b84c2665.js"
+  },
+  {
+    "revision": "7f02a3354be9f75a98ae37a20cf60995",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "817153ec89f160efb372",
+    "url": "/css/app.a8f31159.css"
+  }
+];
